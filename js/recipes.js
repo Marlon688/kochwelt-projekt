17ae9f1aa0c044082edf1eba3dfe3966afbf3762
AngeloDevLab <apietsch94@gmail.com>
2026-03-09 17:16:50 +0100

    exp
    - add recipes.js

---
+++ js/recipes.js
@@ -0,0 +1,47 @@
+
+// Recipes
+
+const recipes = 
+[
+    {
+        id: 0,
+
+        title: "Obstsalat a la Alfons",
+        description: "Eine besondere Interpretation eines Klassikers.",
+
+        duration: "30 Min",
+        difficulty: "simple", // simple, normal, expert
+        published: "xx.xx.20xx",
+
+        servings: 4,
+        ingredients: 
+        [
+            { name: "Orange", amount: 2, unit: "Stk." },
+            { name: "Apfel", amount: 1, unit: "Stk." },
+            { name: "Weintrauben", amount: 100, unit: "g" },
+            { name: "Erdbeeren", amount: 100, unit: "g" },
+            { name: "Honig", amount: 0, unit: "" },
+            { name: "Fruchessig (Himbeer oder Apfel)", amount: 0, unit: "" },
+        ],
+
+        preperation: 
+        [   "Alles Früchte waschen.",
+            "Orange und Apfel in würfel schneiden. Weintrauben und Erdbeeren halbieren.",
+            "Mit Honig und Fruchtessig (Himbeer oder Apfel) abschmecken."
+        ],
+
+        tipps: 
+        [
+            "Du kannst auch andere Früchte nehmen.",
+            "Statts Fruchtessig geht auch noch ein Schluck Orangen- oder Zitronensaft.",
+            "Wer es etwas süßer mag gerne noch mit etwas Vanillezucker abschmecken."
+        ],
+
+        autor: "Alfons"
+    },
+
+
+
+
+
+]
