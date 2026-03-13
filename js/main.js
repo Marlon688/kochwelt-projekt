56b60c1e5769bd7fc6a1605df8a451a87373493a
AngeloDevLab <apietsch94@gmail.com>
2026-03-13 02:38:35 +0100

    add border limit and style improvements

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 const hamMenu = document.getElementById("ham-menu");
 const headerNavLinks = document.getElementById("header-nav-links");
 
-hamMenu.addEventListener("click", () => {
-  hamMenu.classList.toggle("active");
-  headerNavLinks.classList.toggle("active");
-});
+hamMenu.addEventListener("click", toggleMobileNavigation);
+
+function toggleMobileNavigation()
+{
+    hamMenu.classList.toggle("active");
+    headerNavLinks.classList.toggle("active");
+}
 
 //contact-form
 
